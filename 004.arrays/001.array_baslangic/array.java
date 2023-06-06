12838ce0497d39c8bf69778fa4c4d4ed7721d226
Selahattin Güngör <67766746+SelahattinGngr@users.noreply.github.com>
2023-06-06 23:36:19 +0300

    Add files via upload

--- 004.arrays/001.array_baslangic/array.java
+++ 004.arrays/001.array_baslangic/array.java
@@ -15,8 +15,20 @@ public class array
         ogrenciler[0] = "selo"; // programlamada indexler 0 dan baslar
         ogrenciler[1] = "busra";
         ogrenciler[2] = "evrim";
+        // ogrenciler[3] = "onur";
+            /* ogrenciler[3] derlenirken hata olusturur sebebi ise biz array olustururken
+               3 dizilik yer actik */
+                    // new String[3] derken 3 e kadar yeri olacak degil 3 kadar yeri olacak
+                    // yani 0,1,2 seklinde olacak demis bulunmaktayiz.
 
         for (int i = 0; i < ogrenciler.length; i++)
             System.out.println(ogrenciler[i]);
+        System.out.println("\n");
+
+        // ogrenci bizim gecici tmp miz
+        // ogrenciler dizisindeki her bir elemani gezer ve ogrenci tmp'sine atar yazdirir
+        // yaptigi islem yukaridaki for ile ayni islevdedir
+        for (String ogrenci:ogrenciler)
+            System.out.println(ogrenci);
     }
 }
